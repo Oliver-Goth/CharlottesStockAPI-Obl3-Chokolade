f3b8bee94fcbc2ed6bf07196e371d480f53f8e31
Oliver Mikkel Opfermann Göth <oliv8037@edu.zealand.dk>
2024-01-17 23:16:48 +0100

    Add project files.

---
+++ CharlottesStockAPI/Controllers/EasterEggsController.cs
@@ -0,0 +1,67 @@
+﻿using ChocolateLib;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CharlottesStockAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EasterEggsController : ControllerBase
+    {
+        private readonly EasterEggsRepository _repository;
+
+        public EasterEggsController()
+        {
+            _repository = new EasterEggsRepository();
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var eggs = _repository.Get();
+            return Ok(eggs);
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{productNo}")]
+        public IActionResult GetByProductNo(int productNo)
+        {
+            try
+            {
+                var egg = _repository.GetByProductNo(productNo);
+                return Ok(egg);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(new { error = ex.Message });
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet("lowstock/{stockLevel}")]
+        public IActionResult GetLowStock(int stockLevel)
+        {
+            var lowStockEggs = _repository.GetLowStock(stockLevel);
+            return Ok(lowStockEggs);
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut]
+        public IActionResult Update([FromBody] EasterEgg egg)
+        {
+            try
+            {
+                _repository.Update(egg);
+                return NoContent();
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+    }
+}
---
+++ CharlottesStockAPI/Program.cs
@@ -0,0 +1,36 @@
+using ChocolateLib;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+builder.Services.AddSingleton<EasterEggsRepository>(new EasterEggsRepository());
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
